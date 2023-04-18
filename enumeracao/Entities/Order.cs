96ea40d0c3addb127c29828452fee766a1fea2fa
mat86 <mat86@MATHEUZINPC>
2023-04-18 20:38:08 -0300

    Add project files.

---
+++ enumeracao/Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using enumeracao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using enumeracao.Entities;
+
+namespace enumeracao.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+
+        }
+    }
+}
---
+++ enumeracao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using enumeracao.Entities;
+using enumeracao.Entities.Enums;
+
+namespace enumeracao
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os); 
+
+            Console.WriteLine(txt);
+          
+        }
+    }
+}
